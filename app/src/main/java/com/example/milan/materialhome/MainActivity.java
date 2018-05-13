4c1416699e0f9e991e750839961a9c3eac9ad22f
milan400 <tripathimilan28@gmail.com>
2018-05-13 14:39:28 +0545

    First commit

---
+++ app/src/main/java/com/example/milan/materialhome/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.milan.materialhome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
